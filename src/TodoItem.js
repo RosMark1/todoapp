567c6cab0dbbf94f0f6032ad777f5830f929823e
Rosie Mark <rosiemark.ja@gmail.com>
2023-05-21 21:36:57 +0300

    using usestate

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TodoItems.css'
 
 function TodoItem() {
+  const [text, setText] = useState("Take out the trush");
+  
+  const [checked, setChecked] = useState(false);
+  const onChackedboxClick = () => {
+    setChecked(!checked)
+  }
+
   return (
     <div className='todo-item'>
-        <input type="checkbox" />
-        To take out the trash
+        <input type="checkbox" checked={checked} onClick={onChackedboxClick}/>
+        <span className={checked ? 'delete': ""}>{text}</span>
+        
     </div>
   )
 }
